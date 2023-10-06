80c98a9d360e600a8be757df926023ad83dfde2d
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-06 10:54:26 +0200

    Add basics page layout

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'marco';
+  public nameUpper: string = 'MARCO';
+  public fullName: string = 'mArCo aNtOnIo';
 }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -15,9 +15,13 @@ export class MenuComponent {
         label: 'Pipes de Angular',
         icon: 'pi pi-desktop',
         items: [
-          { label: 'Texto y Fechas', icon: 'pi pi-align-left' },
-          { label: 'Numeros', icon: 'pi pi-dollar' },
-          { label: 'No comunes', icon: 'pi pi-globe' },
+          {
+            label: 'Texto y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
+          },
+          { label: 'Numeros', icon: 'pi pi-dollar', routerLink: 'numbers' },
+          { label: 'No comunes', icon: 'pi pi-globe', routerLink: 'uncommon' },
         ],
       },
       {
