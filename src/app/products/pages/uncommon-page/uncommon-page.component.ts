eed2facca4b3678a0c827e99ae3a2c642c616b64
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-10 10:02:00 +0200

    Add slice pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  //i18n Select
+  public name: string = 'Juan';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  //i18Plural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Juan',
+    'Erika',
+    'Fernando',
+    'Melissa',
+  ];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 esperando.',
+    other: 'tenemos # clientes esperando.',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
 }
